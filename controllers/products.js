efc87432a57301593a4cad7443cc87cb0ce90590
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-05-07 18:34:53 +0100

    worked on pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProducts = asyncWrapper(async (req, res) => {
 
 // get specific products
 const getProduct = asyncWrapper(async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, price, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -20,9 +20,29 @@ const getProduct = asyncWrapper(async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-
-  const product = await Product.find(queryObject);
-  res.status(200).json({ product, nbHits: product.length });
+  if (price) {
+    queryObject.price = price;
+  }
+  let result = Product.find(queryObject);
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  //get selected fields
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 });
 
 module.exports = {
